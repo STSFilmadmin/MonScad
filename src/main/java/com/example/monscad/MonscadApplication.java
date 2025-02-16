10c261868e8be989b3e019c7c14886043cb454a5
Vasyl.Boilo <Vasyl.Boilo@gmail.com>
2025-02-16 20:54:35 +0200

    Initial commit: Spring Boot + Docker setup / Початковий коміт: налаштування Spring Boot + Docker

---
+++ src/main/java/com/example/monscad/MonscadApplication.java
@@ -0,0 +1,13 @@
+package com.example.monscad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonscadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonscadApplication.class, args);
+	}
+
+}
